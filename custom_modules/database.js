0ba0faf3ddec8ac4e338cd3adc3c9319824968b3
falconxtreme <falcon.donovan@gmail.com>
2015-02-14 01:03:49 -0500

    mi first change

---
+++ custom_modules/database.js
@@ -0,0 +1,123 @@
+// dependencies windows
+var mysql = require('mysql');
+var queriesInicioBD = require('./crearbdinicial');
+var queriesLeerTablas = require('./leerTablas');
+
+// run query to db
+var dbConfig = {
+   host:'localhost',
+   user:'root',
+   password:''
+};
+
+// run query to db
+var dbConfigWithBD = {
+   host:'localhost',
+   user:'root',
+   password:'',
+   database: 'MercasaTest'
+};
+
+var runQueryIniBD = function (Query) {
+    var connection = mysql.createConnection(dbConfig);
+    connection.connect(function (err) {
+        if (err) throw err;
+    });
+
+    connection.query(Query, function (err) {
+        if (err) {
+            throw err;
+        }
+        else {
+            console.log('Se corrió correctamente el query.');
+        }
+    });
+
+    connection.end();
+}
+
+var runQueryIni = function (Query) {
+    var connection = mysql.createConnection(dbConfigWithBD);
+    connection.connect(function (err) {
+        if (err) throw err;
+    });
+
+    connection.query(Query, function (err) {
+        if (err) {
+            throw err;
+        }
+        else {
+            console.log('Se corrió correctamente el query.');
+        }
+    });
+
+    connection.end();
+}
+
+var runQueryWithoutData = function(Query,callback){
+   var connection = mysql.createConnection(dbConfigWithBD);
+   connection.connect(function(err) {
+      if (err) throw err;
+   });
+   
+   connection.query(Query,function(err, res){
+         if (err) throw err;
+         if (callback){
+            callback(res);
+         }
+      }
+   );
+        connection.end();
+}
+
+var runQueryWithData = function(Query,Data,callback){
+   var connection = mysql.createConnection(dbConfigWithBD);
+   connection.connect(function(err) {
+      if (err) throw err;
+   });
+   
+   connection.query(Query,Data,function(err, res){
+         if (err) throw err;
+         if (callback){
+            callback(res);
+         }
+      }
+   );
+        connection.end();
+}
+
+
+
+// module
+var DB = function(config){
+   config = config || {};
+}
+
+DB.prototype.createDataBase = function(){
+   var Query = queriesInicioBD.SQLCREATEDATABASE;
+   runQueryIniBD(Query);
+}
+
+DB.prototype.createTables = function(){
+   var Query = queriesInicioBD.SQLCREATETABLAS;
+   runQueryIni(Query);
+}
+
+DB.prototype.getAllProducts = function(callback){
+   var Query = queriesLeerTablas.SQLREAD_ALLPRODUCTS;
+   runQueryWithoutData(Query,function(res){
+      //res = res.pop();
+      callback(res);
+   });
+}
+
+/*
+DB.prototype.saveSingleData = function(data){
+   var Query = queries.SQLSAVESINGLEDATA;
+   var Data  = data;
+   runQuery(Query,Data);
+}
+*/
+
+
+module.exports = DB;
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var DATABASE = require('../custom_modules/database');
+var BD = new DATABASE();
+
+var productosBD;
+
+/* 
+BD.getAllProducts(function(productosBD){
+   console.log(productosBD);
+});
+*/
+
+router.get('/', function(req, res, next) {
+    BD.getAllProducts(function(productosBD){
+       console.log(productosBD);
+       res.render('index', { title: 'MERCASA', productos: productosBD});
+    });
+  
+});
+
+/* GET home page. 
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', productos: productosBD});
+});*/
+
+module.exports = router;
